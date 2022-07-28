3a77bfd83d0538a56ee8334edeff112049cc3fc8
T45K <tasktas9@gmail.com>
2022-07-28 23:43:15 +0900

    use the latest junit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,10 +27,7 @@ repositories {
 }
 
 dependencies {
-    // Use the latest Groovy version for Spock testing
-    testImplementation("org.codehaus.groovy:groovy:3.0.9")
-
-    testImplementation("org.junit.jupiter:junit-jupiter:5.5.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0-RC1")
 
     // This dependency is used by the application.
     implementation("com.google.guava:guava:30.1.1-jre")
